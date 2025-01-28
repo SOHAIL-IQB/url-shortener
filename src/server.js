a27e4b0510fd3681b1552f59e1e8a32391cbc266
SOHAIL IQBAL <sohaileqwal@gmail.com>
2025-01-28 20:28:54 +0530

    Initial Commit

--- src/server.js
+++ src/server.js
@@ -1,23 +1,23 @@
-const express = require('express')
-require('dotenv').config()
-require('./db/connect')
-const {v1Router} = require("./routers/v1/index")
-const {RedirectURLController} = require("./controllers/url.controller")
+const express = require("express");
+require("dotenv").config();
+require("./db/connect");
+const { v1Router } = require("./routers/v1/index");
+const { RedirectURLController } = require("./controllers/url.controller");
 
-const NODE_ENV = process.env.NODE_ENV
+const NODE_ENV = process.env.NODE_ENV;
 
-const PORT = process.env[`${NODE_ENV}_PORT`]
+const PORT = process.env[`${NODE_ENV}_PORT`];
 
 // const app = express()
-const server = express()
+const server = express();
 
 // It will parse the body of the request into JSON
-server.use(express.json())
+server.use(express.json());
 
-server.get("/:keyId", RedirectURLController)
+server.get("/:keyId", RedirectURLController);
 
-server.use("/api/v1", v1Router)
+server.use("/api/v1", v1Router);
 
-server.listen(PORT, ()=>{
-    console.log(`${NODE_ENV} Server is started on PORT - ${PORT}`)
-})
+server.listen(PORT, () => {
+  console.log(`${NODE_ENV} Server is started on PORT - ${PORT}`);
+});
