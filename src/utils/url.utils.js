c831a4599d86781f2649219cc0c850d64d315237
SOHAIL IQBAL <sohaileqwal@gmail.com>
2025-01-28 20:28:19 +0530

    Initial Commit

--- src/utils/url.utils.js
+++ src/utils/url.utils.js
@@ -1,13 +1,16 @@
-function GenerateUniqueIdForTheURLUtil(charCount){
-    const possibleCharString = 'ABCDEFGHTFDCXDDFDFCagjsgfjhahjgdjhagdghjagdhdaghj16871361389176361836718'
-    let result = ''
-    for(let i=0; i<charCount;i++){
-        const uniqueIndex = parseInt(Math.random()*(possibleCharString.length-1))
-        result+=possibleCharString[uniqueIndex]
-    }
-    return result
+function GenerateUniqueIdForTheURLUtil(charCount) {
+  const possibleCharString =
+    "ABCDEFGHTFDCXDDFDFCagjsgfjhahjgdjhagdghjagdhdaghj16871361389176361836718";
+  let result = "";
+  for (let i = 0; i < charCount; i++) {
+    const uniqueIndex = parseInt(
+      Math.random() * (possibleCharString.length - 1)
+    );
+    result += possibleCharString[uniqueIndex];
+  }
+  return result;
 }
 
 module.exports = {
-    GenerateUniqueIdForTheURLUtil
-}
+  GenerateUniqueIdForTheURLUtil,
+};
