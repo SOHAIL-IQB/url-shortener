0cf3c0f1e93175bfa7f2e5c56862dacdb9f70e47
SOHAIL IQBAL <sohaileqwal@gmail.com>
2025-01-28 20:02:38 +0530

    Initial Commit

---
+++ src/db/connect.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+require('dotenv').config()
+
+const NODE_ENV = process.env.NODE_ENV
+
+const MONGODB_URL = process.env[`${NODE_ENV}_MONGODB_URL`]
+
+mongoose.connect(MONGODB_URL).then(()=>{
+    console.log(`Connected to ${NODE_ENV} MONGODB`)
+}).catch((err)=>{
+    console.log(`Error while connecting to ${NODE_ENV} MONGODB, with err - ${err}`)
+})
