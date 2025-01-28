be0a0166b679198e0337905a2f39a3048c6a1dd8
SOHAIL IQBAL <sohaileqwal@gmail.com>
2025-01-28 20:27:57 +0530

    Initial Commit

--- src/services/url.service.js
+++ src/services/url.service.js
@@ -1,75 +1,74 @@
-const URLSModel = require("./../models/url.model")
+const URLSModel = require("./../models/url.model");
 
-async function CreateNewURLService(originalUrl, keyId){
-    try{
+async function CreateNewURLService(originalUrl, keyId) {
+  try {
+    const result = await URLSModel.create({
+      originalUrl: originalUrl,
+      keyId: keyId,
+    });
 
-        const result = await URLSModel.create({"originalUrl" : originalUrl, "keyId" : keyId})
-
-        if(!result){
-            throw new Error("unable to call URLSModel.create()")
-        }
-
-        return {
-            success : true,
-            data : result
-        }
-
-    }catch(err){
-
-        console.log(`Error in CreateNewURLService for originalUrl : ${originalUrl} & keyId : ${keyId} `)
-
-        return {
-            success : false
-        }
-
+    if (!result) {
+      throw new Error("unable to call URLSModel.create()");
     }
+
+    return {
+      success: true,
+      data: result,
+    };
+  } catch (err) {
+    console.log(
+      `Error in CreateNewURLService for originalUrl : ${originalUrl} & keyId : ${keyId} `
+    );
+
+    return {
+      success: false,
+    };
+  }
 }
 
-async function GetURLDetailsUsingItsKeyIdService(keyId){
-    try{
+async function GetURLDetailsUsingItsKeyIdService(keyId) {
+  try {
+    const URLDetail = await URLSModel.findOne({ keyId: keyId }).exec();
 
-        const URLDetail = await URLSModel.findOne({"keyId":keyId}).exec()
-
-        if(!URLDetail){
-            throw new Error(`Unable to fetch detail for URL with keyId : ${keyId} `)
-        }
-
-        return {
-            success : true,
-            data : URLDetail
-        }
-
-    }catch(err){
-        console.log(`Error in GetURLDetailsUsingItsKeyIdService with err : ${err}`)
-        return {
-            success : false
-        }
+    if (!URLDetail) {
+      throw new Error(`Unable to fetch detail for URL with keyId : ${keyId} `);
     }
+
+    return {
+      success: true,
+      data: URLDetail,
+    };
+  } catch (err) {
+    console.log(`Error in GetURLDetailsUsingItsKeyIdService with err : ${err}`);
+    return {
+      success: false,
+    };
+  }
 }
 
 async function UpdateTheClickedCountOfURLByOneUsingMongoIdService(mongoId) {
-    try{
+  try {
+    const URL = await URLSModel.findOne({ _id: mongoId }).exec();
 
-        const URL = await URLSModel.findOne({_id:mongoId}).exec()
+    URL.clickedCount = URL.clickedCount + 1;
 
-        URL.clickedCount = URL.clickedCount + 1
+    await URL.save();
 
-        await URL.save()
-
-        return {
-            success : true
-        }
-
-    }catch(err){
-        console.log(`Error in UpdateTheClickedCountOfURLByOneUsingKeyIdService with err : ${err}`)
-        return {
-            success : false
-        }
-    } 
+    return {
+      success: true,
+    };
+  } catch (err) {
+    console.log(
+      `Error in UpdateTheClickedCountOfURLByOneUsingKeyIdService with err : ${err}`
+    );
+    return {
+      success: false,
+    };
+  }
 }
 
 module.exports = {
-    CreateNewURLService,
-    GetURLDetailsUsingItsKeyIdService,
-    UpdateTheClickedCountOfURLByOneUsingMongoIdService
-}
+  CreateNewURLService,
+  GetURLDetailsUsingItsKeyIdService,
+  UpdateTheClickedCountOfURLByOneUsingMongoIdService,
+};
