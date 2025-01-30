79297438412ffde24efcaab022d4dcc890a1a9dd
SOHAIL IQBAL <sohaileqwal@gmail.com>
2025-01-30 10:39:21 +0530

    Added organisation service

--- src/services/organization.service.js
+++ src/services/organization.service.js
@@ -1,2 +1,33 @@
-const ORGANAZATIONSModel = require("./../models/organizations.model");
+const ORGANIZATIONSModel = require("./../models/organizations.model");
 
+const IsOrganizationPresentUsingOrgDomainService = async (
+  organizationDomain
+) => {
+  try {
+    const organization = await ORGANIZATIONSModel.findOne({
+      domain: organizationDomain,
+    }).exec();
+
+    if (organization) {
+      return {
+        success: true,
+        data: organization,
+      };
+    } else {
+      throw new Error(
+        `Unable to get organization details for domain : ${organizationDomain}`
+      );
+    }
+  } catch (err) {
+    console.log(
+      `Error in isOrganizationPresentUsingOrgDomainService with err : ${err}`
+    );
+    return {
+      success: false,
+    };
+  }
+};
+
+module.exports = {
+  IsOrganizationPresentUsingOrgDomainService,
+};
