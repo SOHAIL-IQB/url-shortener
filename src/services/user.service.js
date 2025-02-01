eebf6c532ed26f5afcc55352d8012b53ec98a380
SOHAIL IQBAL <sohaileqwal@gmail.com>
2025-02-01 23:28:01 +0530

    role is added in users.service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -44,7 +44,8 @@ const CreateNewUserService = async (
   fullName,
   email,
   encryptedPassword,
-  organizationId
+  organizationId,
+  organizationRole
 ) => {
   try {
     const user = await USERSModel.create({
@@ -52,6 +53,7 @@ const CreateNewUserService = async (
       email: email,
       password: encryptedPassword,
       organizationId: organizationId,
+      role: organizationRole,
     });
 
     if (user) {
@@ -70,8 +72,29 @@ const CreateNewUserService = async (
   }
 };
 
+const DeleteUserByUserIdService = async (userId) => {
+  try {
+    const user = await USERSModel.findByIdAndDelete(userId).exec();
+
+    if (user) {
+      return {
+        success: true,
+        data: user,
+      };
+    } else {
+      throw new Error(`Unable to delete user with userId : ${userId}`);
+    }
+  } catch (err) {
+    console.log(`Error in DeleteUserByUserIdService with err : ${err}`);
+    return {
+      success: false,
+    };
+  }
+};
+
 module.exports = {
   IsUserPresentUsingEmailService,
   IsUserPresentUsingUserIdService,
   CreateNewUserService,
+  DeleteUserByUserIdService,
 };
