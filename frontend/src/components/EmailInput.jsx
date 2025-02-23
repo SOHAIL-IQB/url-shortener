6c519dd0ea377e9049ce71f4be151e61c583657f
SOHAIL IQBAL <sohaileqwal@gmail.com>
2025-02-23 23:05:00 +0530

    EmailInput Updated

--- frontend/src/components/EmailInput.jsx
+++ frontend/src/components/EmailInput.jsx
@@ -1,16 +1,20 @@
-import './EmailInput.css'
+import "./EmailInput.css";
 
 const EmailInput = (props) => {
-
-  const {setEmail} = props
+  const { setEmail } = props;
 
   return (
     <div className="email-input-container">
-        <label for="email-input">Email</label>
-        <input onChange={(e)=>{setEmail(e.target.value)}} id="email-input" type="email"/>
+      <label for="email-input">Email</label>
+      <input
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        id="email-input"
+        type="email"
+      />
     </div>
-  )
+  );
+};
 
-}
-
-export default EmailInput
+export default EmailInput;
